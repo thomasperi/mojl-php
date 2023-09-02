8c08dc32d3450097f5b3490a8943e9cf26154c82
Thomas Peri <tjperi@gmail.com>
2023-09-02 00:11:53 -0700

    test file for HashCache

---
+++ tests/HashCacheTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace ThomasPeri\Mojl\Test;
+use ThomasPeri\Mojl\Util as Util;
+use PHPUnit\Framework\TestCase as TestCase;
+
+final class HashCacheTest extends TestCase {
+
+	// to-do
+
+}
