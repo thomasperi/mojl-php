ac633b7cb276b249e387f29c1e81fe14ccec9524
Thomas Peri <tjperi@gmail.com>
2023-09-02 09:54:16 -0700

    retro-fied HashCacheTest

--- tests/HashCacheTest.php
+++ tests/HashCacheTest.php
@@ -8,45 +8,58 @@ use PHPUnit\Framework\TestCase as TestCase;
 final class HashCacheTest extends TestCase {
 
 	static private $fooHash = 'C*7Hteo!D9vJXQ3UfzxbwnXaijM~';
+	
+	static private function retro($base, $relFile) {
+		$absFile = $base. '/' . $relFile;
+		$oneSecondAgo = time() - 1000;
+		touch($absFile, $oneSecondAgo, $oneSecondAgo);
+		return $absFile;
+	}
 
 	function test_HashCache_noEntries() {
-		$settings = Options::expand([
-			'base' => __DIR__ . '/HashCacheTest'
-		]);
-		$cache = new HashCache($settings);
+		_Clone::run(__FILE__, function ($base) {
+			$settings = Options::expand([
+				'base' => $base,
+			]);
+			$cache = new HashCache($settings);
 			
-		$internalCache = $cache->getCache();
+			$internalCache = $cache->getCache();
 		
-		$this->assertEquals(json_encode($internalCache->entries), '{}');
+			$this->assertEquals(json_encode($internalCache->entries), '{}');
+		});
 	}
 
 	function test_HashCache_noEntryBeforeCreated() {
-		$settings = Options::expand([
-			'base' => __DIR__ . '/HashCacheTest'
-		]);
-		$cache = new HashCache($settings);
-		$relFile = 'src/foo/foo.txt';
+		_Clone::run(__FILE__, function ($base) {
+			$settings = Options::expand([
+				'base' => $base,
+			]);
+			$cache = new HashCache($settings);
+			$relFile = 'src/foo/foo.txt';
 			
-		$entry = $cache->readExistingEntry($relFile);
-		$this->assertEquals($entry, null);
+			$entry = $cache->readExistingEntry($relFile);
+			$this->assertEquals($entry, null);
+		});
 	}
 
 	function test_HashCache_freshEntryAfterCreated() {
-		$settings = Options::expand([
-			'base' => __DIR__ . '/HashCacheTest'
-		]);
-		$cache = new HashCache($settings);
-		$relFile = 'src/foo/foo.txt';
+		_Clone::run(__FILE__, function ($base) {
+			$settings = Options::expand([
+				'base' => $base,
+			]);
+			$cache = new HashCache($settings);
+			$relFile = 'src/foo/foo.txt';
 			
-		$entryCreated = $cache->createEntry($relFile);
-		$entryRead = $cache->readExistingEntry($relFile);
+			$entryCreated = $cache->createEntry($relFile);
+			$entryRead = $cache->readExistingEntry($relFile);
 
-		$this->assertEquals($entryCreated, $entryRead);
+			$this->assertEquals($entryCreated, $entryRead);
 
-		$this->assertEquals($entryRead->hash, self::$fooHash);
-		$this->assertEquals($entryRead->relFile, $relFile);
+			$this->assertEquals($entryRead->hash, self::$fooHash);
+			$this->assertEquals($entryRead->relFile, $relFile);
 
-		$this->assertTrue($cache->entryIsFresh($entryRead));
+			$this->assertTrue($cache->entryIsFresh($entryRead));
+		});
 	}
 
 	function test_HashCache_noFreshEntryAfterModified() {
@@ -56,15 +69,13 @@ final class HashCacheTest extends TestCase {
 			]);
 			$cache = new HashCache($settings);
 			$relFile = 'src/foo/foo.txt';
+			$absFile = self::retro($base, $relFile);
 			
 			$cache->createEntry($relFile);
 		
 			$entry = $cache->readExistingEntry($relFile);
 			$this->assertTrue($cache->entryIsFresh($entry));
 			
-			sleep(1);
-			
-			$absFile = $base . '/'. $relFile;
 			file_put_contents($absFile, 'bar');
 
 			$entry = $cache->readExistingEntry($relFile);
