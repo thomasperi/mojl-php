7bc05dcf625ade75ccadb1df68a36637f7a29f7d
Thomas Peri <tjperi@gmail.com>
2023-09-02 11:56:11 -0700

    mkdir bugfix

--- src/Util.php
+++ src/Util.php
@@ -97,7 +97,10 @@ class Util {
 	}
 	
 	static function writeFileRecursive($file, $data) {
-		mkdir(dirname($file), 0777, true);
+		$dir = dirname($file);
+		if (!is_dir($dir)) {
+			mkdir($dir, 0777, true);
+		}
 		file_put_contents($file, $data);
 	}
 
