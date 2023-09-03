6ff31374f6bd95373eeacee11dfce2dfa3397e73
Thomas Peri <tjperi@gmail.com>
2023-09-02 20:48:19 -0700

    FileUrlTest

--- tests/FileUrlTest.php
+++ tests/FileUrlTest.php
@@ -20,4 +20,43 @@ final class FileUrlTest extends TestCase {
 		});
 	}
 
+	function test_absoluteWithHashFromAbsFilesystemPath() {
+		_CloneBox::run(__FILE__, function ($base, $box) {
+			$settings = Options::expand(['base' => $base]);
+			$currentTemplate = $base . '/' . 'src/home/foo/foo.tpl.js';
+			$currentPage = '/foo/index.html';
+			$filePath = $base . '/src/home/foo/images/icon.gif';
+			$options = null;
+			$actual = Util::fileUrl($settings, $currentTemplate, $currentPage, $filePath, $options);
+			$expected = '/assets/src/home/foo/images/icon.gif?h=wyCFiYxuNtNh1LgBcIfekOG4Rlw~';
+			$this->assertEquals($actual, $expected);
+		});
+	}
+
+	function test_pageRelativeWithPageRelativeUrlsOption() {
+		_CloneBox::run(__FILE__, function ($base, $box) {
+			$settings = Options::expand(['base' => $base, 'pageRelativeUrls' => true]);
+			$currentTemplate = $base . '/' . 'src/home/foo/foo.tpl.js';
+			$currentPage = '/foo/index.html';
+			$filePath = 'images/icon.gif';
+			$options = null;
+			$actual = Util::fileUrl($settings, $currentTemplate, $currentPage, $filePath, $options);
+			$expected = '../assets/src/home/foo/images/icon.gif?h=wyCFiYxuNtNh1LgBcIfekOG4Rlw~';
+			$this->assertEquals($actual, $expected);
+		});
+	}
+
+	function test_omitHashWhenHashIsFalse() {
+		_CloneBox::run(__FILE__, function ($base, $box) {
+			$settings = Options::expand(['base' => $base, 'pageRelativeUrls' => true]);
+			$currentTemplate = $base . '/' . 'src/home/foo/foo.tpl.js';
+			$currentPage = '/foo/index.html';
+			$filePath = 'images/icon.gif';
+			$options = ['hash' => false];
+			$actual = Util::fileUrl($settings, $currentTemplate, $currentPage, $filePath, $options);
+			$expected = '../assets/src/home/foo/images/icon.gif';
+			$this->assertEquals($actual, $expected);
+		});
+	}
+
 }
